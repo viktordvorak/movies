0a053ffee54740b565e7f22705a243d0336d905e
Dvorak Viktor <dvorakviktor@seznam.cz>
2023-10-21 13:37:51 +0200

    [movies] - project structure

---
+++ src/main/java/cz/dvorakv/configuration/WebConfiguration.java
@@ -0,0 +1,25 @@
+package cz.dvorakv.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+
+    /**
+     *
+     * @param registry
+     * allowedOriginPatterns - jaké cizí URL jsou povoleny
+     * allowCredentials -server mohl posílat "cizí" URL adrese cookies.
+     *
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("**")
+                .allowCredentials(true);
+    }
+
+}
